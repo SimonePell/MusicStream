5ef93b62bc2c71101a445de925342509890ede77
Simone <simonepell03@gmail.com>
2025-06-25 22:32:01 +0200

    prova

--- settings.py
+++ settings.py
@@ -49,8 +49,9 @@ ASGI_APPLICATION = 'music_streaming.asgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=env('DATABASE_URL'),
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
+        ssl_require=not DEBUG
     )
 }
 
