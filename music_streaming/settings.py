98614cc654eb8c31c44171cac5e23f28e9ca3d75
Simone <simonepell03@gmail.com>
2025-06-25 23:27:52 +0200

    Drop django-environ, switch to os.environ + dj-database-url

--- music_streaming/settings.py
+++ music_streaming/settings.py
@@ -2,63 +2,110 @@ import os
 from pathlib import Path
 
 import dj_database_url
-import environ
 
-# 1) Base dir
+# 1) Base directory del progetto
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# 2) Carica il .env solo in locale (Railway non avrà un file .env)
+# 2) Caricamento variabili d’ambiente da .env in locale
+#    In produzione Railway non avrà un file .env
 if not os.environ.get("RAILWAY_ENVIRONMENT"):
-    environ.Env.read_env()
+    from dotenv import load_dotenv
+    load_dotenv(BASE_DIR / ".env")
 
-# 3) Env con casting e default
-env = environ.Env(
-    DEBUG=(bool, False),
-)
+# 3) Sicurezza
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+DEBUG = os.environ.get("DEBUG", "False").lower() in ("1", "true", "yes")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
 
-# 4) Sicurezza
-SECRET_KEY     = env("DJANGO_SECRET_KEY")
-DEBUG          = env("DEBUG")
-ALLOWED_HOSTS  = env.list("DJANGO_ALLOWED_HOSTS", default=[])
-
-# 5) Installed apps, middleware, ecc.
+# 4) Applicazioni installate
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    # ...
-    'accounts',
-    'music',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    # tue app
+    "accounts",
+    "music",
 ]
 
+# 5) Middleware (incluso WhiteNoise per gli static)
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    # ...
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'music_streaming.urls'
-WSGI_APPLICATION = 'music_streaming.wsgi.application'
-ASGI_APPLICATION = 'music_streaming.asgi.application'
+ROOT_URLCONF = "music_streaming.urls"
 
-# 6) Database: usa sempre DATABASE_URL iniettata da Railway
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "music_streaming.wsgi.application"
+ASGI_APPLICATION = "music_streaming.asgi.application"
+
+# 6) Database (usa sempre DATABASE_URL di Railway)
 DATABASES = {
-    'default': dj_database_url.config(
+    "default": dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
-        ssl_require=not DEBUG
+        ssl_require=not DEBUG,
     )
 }
 
-# 7) Internationalization, static, ecc.
-LANGUAGE_CODE = 'it-it'
-TIME_ZONE     = 'Europe/Rome'
-USE_I18N      = True
-USE_TZ        = True
+# 7) Password validation
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
 
-STATIC_URL       = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# 8) Internazionalizzazione
+LANGUAGE_CODE = "it-it"
+TIME_ZONE = "Europe/Rome"
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
 
-# 8) Auth URLs
-AUTH_USER_MODEL     = 'accounts.User'
-LOGIN_URL           = 'accounts:login'
-LOGIN_REDIRECT_URL  = 'music:song-list'
-LOGOUT_REDIRECT_URL = 'accounts:login'
+# 9) Static files & media
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# 10) Default primary key field
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# 11) Autenticazione
+AUTH_USER_MODEL = "accounts.User"
+LOGIN_URL = "accounts:login"
+LOGIN_REDIRECT_URL = "music:song-list"
+LOGOUT_REDIRECT_URL = "accounts:login"
